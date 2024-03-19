b155c5259abd7e05ea6fd152fe57fa84dc8f6097
ManuelLovell <69539934+ManuelLovell@users.noreply.github.com>
2024-03-19 19:13:30 +0000

    Build: (f75f628) UpdatableMessages

--- assets/whatsnew-6KVGgSKG.js
+++ assets/whatsnew-6KVGgSKG.js
@@ -1,5 +1,8 @@
-import{O as e,C as o}from"./bsConstants-CIv9LX59.js";const n=document.querySelector("#bs-whatsnew"),s=document.querySelector("#bs-whatsnew-notes");n.innerHTML=`
+import{O as e,C as o}from"./bsConstants-CIv9LX59.js";const s=document.querySelector("#bs-whatsnew"),n=document.querySelector("#bs-whatsnew-notes");s.innerHTML=`
   <div id="newsContainer">
+    <h1>Short Rest! 3/19</h1>
+    Changed the message text to not show at all by default, UNLESS you didn't type any pause text in.
+    </br> Also updated it so you can send new messages while paused.  Should help with some use cases I've heard.
     <h1>Short Rest! 3/6</h1>
     Minor bug where a player could refresh and be no longer paused. Fixed.
     </br>
@@ -7,7 +10,7 @@ import{O as e,C as o}from"./bsConstants-CIv9LX59.js";const n=document.querySelec
     I wanted to do something new.
     </br>
   </div>
-`;e.onReady(async()=>{s.innerHTML=`
+`;e.onReady(async()=>{n.innerHTML=`
     <div id="footButtonContainer">
         <a href="https://www.patreon.com/battlesystem" target="_blank">Patreon!</a>
         <a href="https://discord.gg/ANZKDmWzr6" target="_blank">OBR Discord!</a>
